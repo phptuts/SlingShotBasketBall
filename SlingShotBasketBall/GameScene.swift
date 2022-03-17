d6e35f1d671334d52b527d2e33f89267b7790856
phptuts <noah@kittyhawk.io>
2022-03-16 23:26:31 -0400

    Got the rubber band to dynamically draw

--- SlingShotBasketBall/GameScene.swift
+++ SlingShotBasketBall/GameScene.swift
@@ -18,6 +18,8 @@ class GameScene: SKScene {
     
     var basketballSprite: SKSpriteNode!
     
+    var rubberBand: SKShapeNode?
+    
     override func didMove(to view: SKView) {
         basketballSprite = self.childNode(withName: "basketball") as? SKSpriteNode
         
@@ -41,10 +43,24 @@ class GameScene: SKScene {
         }
         guard let touch = touches.first else { return }
         basketballSprite.position = touch.location(in: self)
+        
+        rubberBand?.removeFromParent()
+        let path = CGMutablePath()
+        path.move(to: basketballSprite.position)
+        path.addLine(to: CGPoint(x: 200, y: 300))
+        
+
+        rubberBand = SKShapeNode()
+        rubberBand?.path = path
+        rubberBand?.strokeColor = .purple
+        rubberBand?.lineWidth = 30
+        addChild(rubberBand!)
+        
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-         moveBasketball = false
+        moveBasketball = false
+        rubberBand?.removeFromParent()
     }
     
     
