e08e10870bc8e2fa978b8ed89c2e3d9cbfc35bee
phptuts <noah@kittyhawk.io>
2022-03-18 00:07:57 -0400

    figured out how to calculate the angle

--- SlingShotBasketBall/GameScene.swift
+++ SlingShotBasketBall/GameScene.swift
@@ -20,8 +20,14 @@ class GameScene: SKScene {
     
     var rubberBand: SKShapeNode?
     
+    var ballInMotion = false
+    
+    let slingshotPoint = CGPoint(x: 200, y: 300)
+    
     override func didMove(to view: SKView) {
         basketballSprite = self.childNode(withName: "basketball") as? SKSpriteNode
+        basketballSprite.physicsBody = SKPhysicsBody(circleOfRadius: 20)
+        basketballSprite.physicsBody?.affectedByGravity = false
         
     }
     
@@ -38,7 +44,7 @@ class GameScene: SKScene {
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if !moveBasketball {
+        if !moveBasketball || ballInMotion {
             return
         }
         guard let touch = touches.first else { return }
@@ -47,7 +53,7 @@ class GameScene: SKScene {
         rubberBand?.removeFromParent()
         let path = CGMutablePath()
         path.move(to: basketballSprite.position)
-        path.addLine(to: CGPoint(x: 200, y: 300))
+        path.addLine(to: slingshotPoint)
         
 
         rubberBand = SKShapeNode()
@@ -59,12 +65,55 @@ class GameScene: SKScene {
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        
+        
         moveBasketball = false
+        
+        let basketballPoint = basketballSprite.position
+
+        let velocity = 10
+        
+        
+        print("\(180 - basketballPoint.angle(to: slingshotPoint)) angle")
+        
         rubberBand?.removeFromParent()
     }
     
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
+    }
+}
+
+
+// https://stackoverflow.com/a/58137779
+//extension CGPoint {
+//    func angle( ending: CGPoint) -> CGFloat {
+//        let center = CGPoint(x: ending.x - self.x, y: ending.y - self.y)
+//        let radians = atan2(center.y, center.x)
+//        let degrees = radians * 180 / .pi
+//        return degrees > 0 ? degrees : degrees + degrees
+//    }
+//}
+
+extension CGFloat {
+    var degrees: CGFloat {
+        return self * CGFloat(180) / .pi
+    }
+}
+
+extension CGPoint {
+    func angle(to comparisonPoint: CGPoint) -> CGFloat {
+        let originX = comparisonPoint.x - x
+        let originY = comparisonPoint.y - y
+        let bearingRadians = atan2f(Float(originY), Float(originX))
+        var bearingDegrees = CGFloat(bearingRadians).degrees
+
+        while bearingDegrees < 0 {
+            bearingDegrees += 360
+        }
+
+        return bearingDegrees
     }
 }
